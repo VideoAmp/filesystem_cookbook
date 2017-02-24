530dcb4f7988638267ef636e0fbaea4e9e7f75d5
Eric Lakich <eric.lakich@gmail.com>
2017-02-24 10:31:52 -0800

    adding i2 instance type

---
+++ recipes/cass-i2-jbod.rb
@@ -0,0 +1,34 @@
+# We want to support LVM
+include_recipe "lvm"
+
+mount "/media/ephemeral0" do
+  only_if { ::File.exist?("/media/ephemeral0") }
+  device "/dev/xvdb"
+  action :umount
+end
+
+filesystem "cassandra" do
+  fstype "ext4"
+  device "/dev/sda1"
+  mount "/var/lib/cassandra"
+  user "cassandra"
+  group "cassandra"
+  ignore_existing true
+  force true
+  action [:create, :enable, :mount]
+end
+
+directory '/var/lib/cassandra/data' do
+  owner "cassandra"
+  group "cassandra"
+  mode '0755'
+  action :create
+end
+
+directory '/var/lib/cassandra/commitlog' do
+  owner "cassandra"
+  group "cassandra"
+  mode '0755'
+  action :create
+end
+
